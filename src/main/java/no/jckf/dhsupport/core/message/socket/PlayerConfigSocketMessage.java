62a9accebaf50727c86e29f4378dc2ffd185c07f
Jim C K Flaten <jckf@jckf.no>
2024-04-16 04:44:38 +0200

    Config options.

--- src/main/java/no/jckf/dhsupport/core/message/socket/PlayerConfigSocketMessage.java
+++ src/main/java/no/jckf/dhsupport/core/message/socket/PlayerConfigSocketMessage.java
@@ -20,6 +20,8 @@ package no.jckf.dhsupport.core.message.socket;
 
 import no.jckf.dhsupport.core.bytestream.Decoder;
 import no.jckf.dhsupport.core.bytestream.Encoder;
+import no.jckf.dhsupport.core.configuration.Configuration;
+import no.jckf.dhsupport.core.configuration.DhsConfig;
 
 public class PlayerConfigSocketMessage extends SocketMessage
 {
@@ -65,5 +67,31 @@ public class PlayerConfigSocketMessage extends SocketMessage
         this.generateMultipleDimensions = decoder.readBoolean();
     }
 
-    // TODO: Setters and getters.
+    public void fromConfiguration(Configuration config)
+    {
+        this.renderDistance = config.getInt(DhsConfig.RENDER_DISTANCE);
+        this.distantGenerationEnabled = config.getBool(DhsConfig.DISTANT_GENERATION_ENABLED);
+        this.fullDataRequestConcurrencyLimit = config.getInt(DhsConfig.FULL_DATA_REQUEST_CONCURRENCY_LIMIT);
+        this.generationTaskPriorityRequestRateLimit = config.getInt(DhsConfig.GENERATION_TASK_PRIORITY_REQUEST_RATE_LIMIT);
+        this.realTimeUpdatedEnabled = config.getBool(DhsConfig.REAL_TIME_UPDATES_ENABLED);
+        this.loginDataSyncEnabled = config.getBool(DhsConfig.LOGIN_DATA_SYNC_ENABLED);
+        this.loginDataSyncRateConcurrencyLimit = config.getInt(DhsConfig.LOGIN_DATA_SYNC_RC_LIMIT);
+        this.generateMultipleDimensions = config.getBool(DhsConfig.GENERATE_MULTIPLE_DIMENSIONS);
+    }
+
+    public Configuration toConfiguration()
+    {
+        Configuration config = new Configuration();
+
+        config.set(DhsConfig.RENDER_DISTANCE, this.renderDistance);
+        config.set(DhsConfig.DISTANT_GENERATION_ENABLED, this.distantGenerationEnabled);
+        config.set(DhsConfig.FULL_DATA_REQUEST_CONCURRENCY_LIMIT, this.fullDataRequestConcurrencyLimit);
+        config.set(DhsConfig.GENERATION_TASK_PRIORITY_REQUEST_RATE_LIMIT, this.generationTaskPriorityRequestRateLimit);
+        config.set(DhsConfig.REAL_TIME_UPDATES_ENABLED, this.realTimeUpdatedEnabled);
+        config.set(DhsConfig.LOGIN_DATA_SYNC_ENABLED, this.loginDataSyncEnabled);
+        config.set(DhsConfig.LOGIN_DATA_SYNC_RC_LIMIT, this.loginDataSyncRateConcurrencyLimit);
+        config.set(DhsConfig.GENERATE_MULTIPLE_DIMENSIONS, this.generateMultipleDimensions);
+
+        return config;
+    }
 }
